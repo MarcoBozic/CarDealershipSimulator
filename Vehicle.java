a2d010110b8972369053c7a1008a85c83138a6d6
Marco <bozicma@gmail.com>
2019-03-14 22:45:56 -0400

    added comments for part 1

--- Vehicle.java
+++ Vehicle.java
@@ -6,11 +6,15 @@ public class Vehicle
     private int numWheels;
     private boolean answer;
 
+    //enum used to set static final value for ELECTRIC_MOTOR and GAS_ENGINE
     enum PowerSource
     {
         ELECTRIC_MOTOR, GAS_ENGINE;
     }
+    
+    //creating reference to enum so it can be assigned to the constructer
     public PowerSource power;
+    
     /**
      * Constructor: initializes 
      */
@@ -22,27 +26,46 @@ public class Vehicle
         this.numWheels = numWheels;
     }
     
-
+    /**
+       Returns the power source
+       @return the power source
+     */
     public PowerSource getPower()
     {
         return power;
     }
     
+    /**
+       Returns the manufacturer
+       @return the manufacturer
+     */
     public String getMfr()
     {
         return mfr;
     }
 
+    /**
+       Returns the color
+       @return the color
+     */
     public String getColor()
     {
         return color;
     }
 
+    /**
+       Returns the number of wheels
+       @return the number of wheels
+     */
     public int getNumWheels()
     {
         return numWheels;
     }
 
+    /**
+       Returns the boolean value true or false if vehicles are equal
+       @return answer
+     */
     public boolean equal(Vehicle other)
     {
         if (this.getPower() == other.getPower() && this.getMfr() == other.getMfr() && this.getNumWheels() == other.getNumWheels())
@@ -56,6 +79,10 @@ public class Vehicle
         return answer;
     }
 
+    /**
+       Returns the string containing manufacturer name and color seperated by a space
+       @return the display
+     */
     public String display()
     {
         return mfr + " " + color; 
