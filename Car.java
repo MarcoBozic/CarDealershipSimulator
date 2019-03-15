ef91a6c232d68d40dca8e81de87c5031ca1aadd3
Marco <bozicma@gmail.com>
2019-03-15 13:14:57 -0400

    created instance variables

---
+++ Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle
+{
+    private String model;
+    private int maxRange;
+    private double safetyRating;
+    private boolean AWD;
+    private double price;
+    
+}
