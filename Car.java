b15081aa0d3c6be53bcf2af6b8c8d6670040f774
Marco <bozicma@gmail.com>
2019-03-15 15:06:51 -0400

    created constructor method

--- Car.java
+++ Car.java
@@ -1,9 +1,27 @@
 public class Car extends Vehicle
 {
-    private String model;
+    //private String model;
     private int maxRange;
     private double safetyRating;
     private boolean AWD;
     private double price;
-    
+
+    enum CarModel
+    {
+        SEDAN,SUV,SPORTS,MINIVAN;
+    }
+
+    public CarModel model;
+
+    public Car(CarModel model, int maxRange, double safetyRating, boolean AWD, double price, String mfr, String color, PowerSource power, int numWheels)
+    {
+        super(mfr,color,power,numWheels);
+        this.model = model;
+        this.maxRange = maxRange;
+        this.safetyRating = safetyRating;
+        this.AWD = AWD;
+        this.price = price;
+    }
+
+
 }
